274f7d0d50e95b59d445f7240e5d69bbcaae14f0
oplingerpaul-cloud <oplingerpaul@gmail.com>
2026-01-08 17:20:48 +0000

    ec2 with security group

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "example1" {
   ami           = var.mathi-ami
   instance_type = var.vm-size
+# attaching security gourp to ec2 vm
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = var.vm-name
